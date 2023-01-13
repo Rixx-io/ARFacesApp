1c0b9c1f38ab546b8f122b409fbd975d20926da7
Joe Holt <1596163+joe07734@users.noreply.github.com>
2023-01-13 16:15:28 -0500

    better

--- ARKitFaceExample/Face Overlay Content/TransformVisualization.swift
+++ ARKitFaceExample/Face Overlay Content/TransformVisualization.swift
@@ -12,6 +12,9 @@ import Foundation
 class TransformVisualization: NSObject, VirtualContentController {
     var contentNode: SCNNode?
 
+    let destIP = "192.168.50.25"
+    let destPort: UInt16 = 54326
+
     // Load multiple copies of the axis origin visualization for the transforms this class visualizes.
     lazy var rightEyeNode = SCNReferenceNode(named: "coordinateOrigin")
     lazy var leftEyeNode = SCNReferenceNode(named: "coordinateOrigin")
@@ -23,10 +26,12 @@ class TransformVisualization: NSObject, VirtualContentController {
         
         // Load an asset from the app bundle to provide visual content for the anchor.
         contentNode = SCNReferenceNode(named: "coordinateOrigin")
-        
+
         // Add content for eye tracking in iOS 12.
         self.addEyeTransformNodes()
-        
+
+        self.udpSetup(address: destIP, inport: destPort)
+
         // Provide the node to ARKit for keeping in sync with the face anchor.
         return contentNode
     }
@@ -42,21 +47,24 @@ class TransformVisualization: NSObject, VirtualContentController {
         // let pitch = asin(-faceAnchor.transform[2][1]) * (180/3.14156)
         // let yaw = atan2(faceAnchor.transform[2][0], faceAnchor.transform[2][2]) * (180/3.14156)
         // let roll = atan2(faceAnchor.transform[0][1], faceAnchor.transform[1][1]) * (180/3.14156)
-        self.udpSend(textToSend: faceAnchor.description, address: "192.168.1.126", inport: 54326)
+        self.udpSend(textToSend: faceAnchor.description, address: "172.20.10.2", inport: 54326)
     }
     
     func addEyeTransformNodes() {
         guard #available(iOS 12.0, *), let anchorNode = contentNode else { return }
         
+        anchorNode.simdPivot = float4x4(diagonal: [3, 3, 3, 5])
+
         // Scale down the coordinate axis visualizations for eyes.
-        rightEyeNode.simdPivot = float4x4(diagonal: [3, 3, 3, 1])
-        leftEyeNode.simdPivot = float4x4(diagonal: [3, 3, 3, 1])
+        // rightEyeNode.simdPivot = float4x4(diagonal: [3, 3, 3, 1])
+        // leftEyeNode.simdPivot = float4x4(diagonal: [3, 3, 3, 1])
         
-        anchorNode.addChildNode(rightEyeNode)
-        anchorNode.addChildNode(leftEyeNode)
+        // anchorNode.addChildNode(rightEyeNode)
+        // anchorNode.addChildNode(leftEyeNode)
     }
 
     var fd: Int32 = 0
+    var dest = sockaddr_in()
 
     deinit {
         if (fd != 0) {
@@ -64,28 +72,27 @@ class TransformVisualization: NSObject, VirtualContentController {
         }
     }
 
-    func udpSend(textToSend: String, address: String, inport: UInt16) {
-        if (fd == 0) {
-            fd = socket(AF_INET, SOCK_DGRAM, 0) // DGRAM makes it UDP
-        }
+    func udpSetup(address: String, inport: UInt16) {
+        fd = socket(AF_INET, SOCK_DGRAM, 0) // DGRAM makes it UDP
 
         var port = inport
         if (NSHostByteOrder() == NS_LittleEndian) {
             port = NSSwapShort(port)
         }
 
-        var addr = sockaddr_in()
-        addr.sin_len = UInt8(MemoryLayout.size(ofValue: addr))
-        addr.sin_family = sa_family_t(AF_INET)
-        addr.sin_addr.s_addr = inet_addr(address)
-        addr.sin_port = port
+        dest.sin_len = UInt8(MemoryLayout.size(ofValue: dest))
+        dest.sin_family = sa_family_t(AF_INET)
+        dest.sin_addr.s_addr = inet_addr(address)
+        dest.sin_port = port
+    }
 
+    func udpSend(textToSend: String, address: String, inport: UInt16) {
         textToSend.withCString { cstr -> () in
-            var localCopy = addr
+            var localCopy = dest
 
             withUnsafePointer(to: &localCopy) { pointer -> () in
                 let memory = UnsafeRawPointer(pointer).bindMemory(to: sockaddr.self, capacity: 1)
-                sendto(fd, cstr, strlen(cstr), 0, memory, socklen_t(addr.sin_len))
+                sendto(fd, cstr, strlen(cstr), 0, memory, socklen_t(dest.sin_len))
             }
         }
     }
